6e4df6fc7a44c66faa82b0f2ae1f89817e063331
britfel <pfelipe.brito@gmail.com>
2020-09-22 15:33:26 -0300

    segundo Commit

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+//alert("olá seja bem vindo aluno Gama");
+var canal="Gama Academy";
+var Curso="Curso de Java Script";
+var separador=" - ";
+//alert(canal+serparador+Curso);
+var nome=prompt("Entre com o seu nome","Digite aqui o seu nome");
+alert("Bem Vindo "+ nome+ separador +canal+separador+Curso) ;
+function funcao1(){
+    alert("Idiota");
+}
+function funcao2(){
+    alert("Imbecil");
+}
